c763e32876607bedf493cd369c16b80b57b290c2
nicolascozom <nicolascozom@gmail.com>
2021-03-08 21:12:02 -0300

    emprolijo código

--- public/js/products.js
+++ public/js/products.js
@@ -1,61 +1,62 @@
 window.onload = function () {
 
-    let ultimos = []
+    let ultimos = [] // PARA TRABAJAR DENTRO DEL FOR
 
-    let productos = ''
+    let productos = '' // PARA GUARDAR EL RESULTADO DE CADA ITERACIÓN, Y LUEGO PASARLE AL innerHTML
 
-    let ultimosEnVista = document.getElementById("latest-products")
+    let ultimosEnVista = document.getElementById("latest-products") // RECIBE STRING DE RESULTADOS ACUMULADOS Y CAMBIA SU innerHTML
 
-    axios.get('http://localhost:3000/api/products/latest')
+    axios.get('http://localhost:3000/api/products/latest') // TRAIGO LOS PRODUCTOS
 
     .then(function(res){
         if (res.status == 200){
 
-            for (let i = 0 ; i < 8 ; i ++){
+            for (let i = 0 ; i < 8 ; i ++){ //RECORRO LOS PRIMEROS 8
 
                 ultimos = res.data.data.ultimos;
 
                 let producto = ''
 
-                producto = '<div class="col-12 col-sm-6 col-lg-3">'
-
-                producto += '<section class="product-box">'
-
-                producto += '<a href="/products/detail/' + ultimos[i].id + '">'
-
-                producto += '<figure class="product-box_image">'
-
-                producto += '<img src="/images/products/' + ultimos[i].image + '"' + 'alt="' + ultimos[i].name + '">'
-
-                producto += '</figure>'
-
-                producto += '<article class="product-box_data">'
-
-                producto += '<h2>' + ultimos[i].price + '</h2>'
-
-                producto += '<span>' + ultimos[i].discount + "% OFF" + '</span>'
-
-                producto += '<p>' + ultimos[i].name + '</p>'
-
-                producto += '<i class="fas fa-truck"></i>'
-
-                producto += '</article>'
-
-                producto += '</a>'
-
-                producto += '</section>'
-
-                producto += '</div>'
+                producto = '<div class="col-12 col-sm-6 col-lg-3">' + 
                 
-                productos += producto
+                '<section class="product-box">' + 
+                
+                '<a href="/products/detail/' + ultimos[i].id + '">' + 
+                
+                '<figure class="product-box_image">' + 
+                
+                '<img src="/images/products/' + ultimos[i].image + '"' + 'alt="' + ultimos[i].name + '">' + 
+                
+                '</figure>' + 
+                
+                '<article class="product-box_data">' + 
+                
+                '<h2>' + ultimos[i].price + '</h2>' + 
+                
+                '<span>' + ultimos[i].discount + "% OFF" + '</span>' + 
+                
+                '<p>' + ultimos[i].name + '</p>' + 
+                
+                '<i class="fas fa-truck"></i>' + 
+                
+                '</article>' + 
+                
+                '</a>' + 
+                
+                '</section>' + 
+                
+                '</div>'
+                
+                productos += producto // GUARDO EL HTML CREADO EN ESTA ITERACIÓN
 
             }
 
-            ultimosEnVista.innerHTML = productos
+            ultimosEnVista.innerHTML = productos // PASO EL RESULTADO FINAL AL HTML
 
         }else{
-            // console.log(res.status)
-            console.log(res.data.data.ultimos[0].name)
+            
+            console.log(res.status)
+
         }
     })
     
